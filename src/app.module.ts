de3328451f227cf20a63443b88fd5728433af182
TrompeurC <13604811047@163.com>
2025-02-12 22:34:02 +0800

    feat: 数据库配置，env配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory(config: ConfigService) {
+        return {
+          type: 'mysql',
+          host: config.get<string>('MYSQL_HOST'),
+          port: config.get<number>('MYSQL_PORT'),
+          username: config.get<string>('MYSQL_USER'),
+          password: config.get<string>('MYSQL_PASSWORD'),
+          database: config.get<string>('MYSQL_DATABASE'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          charset: 'utf8mb4',
+          logging: true,
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  // 设置全局api前缀
+  app.setGlobalPrefix('/api');
+  // 添加全局管道
+  app.use(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
